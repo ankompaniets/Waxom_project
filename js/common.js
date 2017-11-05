4d536f01c57e2ae915bff47d79041a9589a259c2
Andrey <whitehang18@gmail.com>
2017-11-06 00:37:11 +0300

    06,11,2017

--- js/common.js
+++ js/common.js
@@ -9,16 +9,25 @@ $ (function() {
  });
 
 $(document).ready(function() {
-  $(window).onscroll = function() {
-    scrollFunction();
+  window.onscroll = function() {
+      scrollFunction();
   };
 });
 
 function scrollFunction() {
-  var scrollPosition = 20;
+  var scrollPosition = 10;
     if (document.body.scrollTop > scrollPosition || document.documentElement.scrollTop > scrollPosition) {
-        $('.js-main-header-container').addClass("js-main-header_scrolled");
+        $('.js-main-header-container').addClass("main-header_scrolled");
+        $('.js-main-logo__img').addClass("main-logo__img_scrolled");
+        $('.js-main-logo__link').addClass("main-logo__link_scrolled");
+        $('.js-main-header-menu-item__link').addClass("main-header-menu-item__link_scrolled");
+        $('.js-main-header-image-item__link').addClass("main-header-image-item__link_scrolled");
+
     } else {
-        $('.js-main-header-container').removeClass("js-main-header_scrolled");
+        $('.js-main-header-container').removeClass("main-header_scrolled");
+        $('.js-main-logo__img').removeClass("main-logo__img_scrolled");
+        $('.js-main-logo__link').removeClass("main-logo__link_scrolled");
+        $('.js-main-header-menu-item__link').removeClass("main-header-menu-item__link_scrolled");
+        $('.js-main-header-image-item__link').removeClass("main-header-image-item__link_scrolled");
     }
 }
