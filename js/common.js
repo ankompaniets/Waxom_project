53e83f9533d06e0b0d3a335d6d7dbe4148f9559f
Andrey <whitehang18@gmail.com>
2017-11-05 13:30:10 +0300

    05.11.2017

--- js/common.js
+++ js/common.js
@@ -1,15 +1,24 @@
 $ (function() {
      $('.fa-bars') .on('click', function() {
 
-            $('.main-header-menu') .slideToggle(300, function() {
-
-                if($(this).css('display') === 'none') {
-                    $(this).removeAttr('style');
-                }
+            $('.main-header-menu_mobile') .slideToggle(300, function() {               
 
             });
-
      });
 
-
  });
+
+$(document).ready(function() {
+  $(window).onscroll = function() {
+    scrollFunction();
+  };
+});
+
+function scrollFunction() {
+  var scrollPosition = 20;
+    if (document.body.scrollTop > scrollPosition || document.documentElement.scrollTop > scrollPosition) {
+        $('.js-main-header-container').addClass("js-main-header_scrolled");
+    } else {
+        $('.js-main-header-container').removeClass("js-main-header_scrolled");
+    }
+}
